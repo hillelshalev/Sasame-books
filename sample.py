722ac261f798f0f354a6dc297d9816486eba3b0c
hillelshalev <143802901+hillelshalev@users.noreply.github.com>
2023-09-09 09:19:25 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import random
 
-# Rhyming story templates with placeholders
+# Adjusted rhyming story templates with placeholders
 templates = [
-    "{name} was in the {place}, feeling quite {emotion}.\nWith their {animal}, they set things in motion.\nThey found a {object}, shining so bright.\nDecided to {action}, with all their might.\nThrough challenges and adventures, they did fare.\nAnd in the end, they learned about {message} with care.",
+    "{name} was in the {place}, feeling quite {emotion}.\nWith their {animal}, adventure was set in motion.\nThey discovered a {object}, gleaming so bright.\nChose to {action}, with courage and might.\nJourneying far, facing challenges rare.\nThey learned about {message}, with wisdom to share.",
     
-    "In the {place}, under the sun's glow,\n{name} and their {animal}, put up a great show.\nA mysterious {object}, caught their keen eye.\nThey chose to {action}, reaching for the sky.\nWith every step, they grew more aware.\nThat the true treasure was the lesson on {message} they'd share.",
+    "In the heart of {place}, as the sun began to glow,\n{name} and their {animal}, prepared for the show.\nA {object} they found, with mysteries to pry.\nThey decided to {action}, aiming for the sky.\nThrough twists and turns, their spirit did flare.\nWith the lesson of {message}, for all to be aware.",
     
-    "{place} held a secret, waiting to be found.\nWhere {name} and their {animal} played around.\nDiscovering a {object}, they felt a rare thrill.\nAnd decided to {action}, testing their skill.\nTheir journey taught them, with every square.\nThe importance of {message}, beyond compare."
+    "{place} whispered tales, of treasures profound.\nWhere {name} and their {animal} were destiny-bound.\nAn {object} they spotted, ancient and still.\nWith determination, they chose to {action} with will.\nWith every challenge, their bond did prepare.\nThe essence of {message}, became their core prayer."
 ]
 
 # Possible values for the placeholders
@@ -26,21 +26,17 @@ messages = {
 heroes = ["hero", "heroine"]
 
 def generate_story(name, hero, place, animal, message_key):
-    while True:  # Keep generating until the story is within the desired word count
-        template = random.choice(templates)
-        story = template.format(
-            name=name,
-            hero=hero,
-            place=place,
-            emotion=random.choice(emotions),
-            object=random.choice(objects),
-            action=random.choice(actions),
-            animal=animal,
-            message=messages[message_key]
-        )
-        word_count = len(story.split())
-        if 190 <= word_count <= 210:
-            break
+    template = random.choice(templates)
+    story = template.format(
+        name=name,
+        hero=hero,
+        place=place,
+        emotion=random.choice(emotions),
+        object=random.choice(objects),
+        action=random.choice(actions),
+        animal=animal,
+        message=messages[message_key]
+    )
     return story
 
 # Streamlit interface
@@ -56,3 +52,4 @@ if st.button("Generate a Story"):
     # Bold the name of the hero
     story = story.replace(name, f"**{name}**")
     st.markdown(story)
+
